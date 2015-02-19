c27ed839a6e951f7fde834ae4652740133820fcc
Quentin L-EILDE <ql-eilde@e2r3p21.42.fr>
2015-02-19 20:16:56 +0100

    ajout j03

---
+++ rendu/j03/ex03/cookie_crisp.php
@@ -0,0 +1,8 @@
+<?php
+if ($_GET["action"] == "set")
+	setcookie($_GET["name"], $_GET["value"]);
+if ($_GET["action"] == "get")
+	echo $_COOKIE[$_GET["name"]]."\n";
+if ($_GET["action"] == "del")
+	setcookie($_GET["name"], $_COOKIE[$_GET["name"]], time());
+?>
---
+++ rendu/j03/ex04/raw_text.php
@@ -0,0 +1,4 @@
+<?php
+header("HTTP/5.0");
+?>
+<html><body>Hello</body></html>
