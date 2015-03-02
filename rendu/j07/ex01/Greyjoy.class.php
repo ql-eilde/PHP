44dc3f7dd4ded6a8dfe11d354c44e5e7ba6c66c1
Quentin L-EILDE <ql-eilde@e2r5p22.42.fr>
2015-03-02 21:08:02 +0100

    cool

---
+++ rendu/j07/ex01/Greyjoy.class.php
@@ -0,0 +1,16 @@
+<?php
+class Greyjoy
+{
+	protected $familyMotto = 'We do not sow';
+
+	public function __construct()
+	{
+		return ;
+	}
+
+	public function __destruct()
+	{
+		return ;
+	}
+}
+?>
---
+++ rendu/j07/ex03/House.class.php
@@ -0,0 +1,15 @@
+<?php
+abstract class House
+{
+	abstract public function getHouseName();
+
+	abstract public function getHouseMotto();
+
+	abstract public function getHouseSeat();
+
+	public function introduce()
+	{
+		print('House '.static::getHouseName().' of '.static::getHouseSeat().' : "'.static::getHouseMotto().'"'.PHP_EOL);
+	}
+}
+?>
